014e1f610f1950b462bdee96e74ae079eb83a4a9
sharonamarachi <79338031+sharonamarachi@users.noreply.github.com>
2023-11-23 13:31:08 +0000

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,36 @@
+package Project;
+
+public class Person {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public Person() {
+        this("");
+    }
+
+    public Person(String name) {
+        this(name, "", "", "");
+    }
+
+    public Person(String name, String address) {
+        this(name, address, "", "");
+    }
+
+    public Person(String name, String address, String phoneNumber) {
+        this(name, address, phoneNumber, "");
+    }
+
+    public Person(String name, String address, String phoneNumber, String email) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String toString() {
+        return String.format(" Name: %s%n Address: %s%n Phone: %s%n Email: %s", this.name, this.address, this.phoneNumber, this.email);
+    }
+}
+
