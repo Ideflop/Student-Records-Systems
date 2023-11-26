43bd2732455710a3a590549872410abf7c6aeb70
Ideflop <vi03pl@gmail.com>
2023-11-26 10:18:24 +0000

    Update Student classes

--- Masters.java
+++ Masters.java
@@ -1,7 +1,5 @@
-public class Masters extends Student
-{
-    public Masters(String name, String address, String phoneNumber, String email, int idNumber)
-    {
-        super(name, address, phoneNumber, email, idNumber, "Masters");
+public class Masters extends Student {
+    public Masters(String name, String address, String phoneNumber, String email, int idNumber, String program) {
+        super(name, address, phoneNumber, email, idNumber, "Masters", program);
     }
 }
--- Undergraduate.java
+++ Undergraduate.java
@@ -1,7 +1,5 @@
-public class Undergraduate extends Student
-{
-    public Undergraduate(String name, String address, String phoneNumber, String email, int idNumber)
-    {
-        super(name, address, phoneNumber, email, idNumber, "Undergraduate");
+public class Undergraduate extends Student {
+    public Undergraduate(String name, String address, String phoneNumber, String email, int idNumber, String program) {
+        super(name, address, phoneNumber, email, idNumber, "Undergraduate", program);
     }
 }
