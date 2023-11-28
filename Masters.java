bd8363c6bacfaa4e0846c3981d30c702af5297b2
Phoenixking900 <87982646+Phoenixking900@users.noreply.github.com>
2023-11-28 17:09:29 +0000

    Update Masters.java

--- Masters.java
+++ Masters.java
@@ -1,3 +1,12 @@
+/**
+     * Constructs a `Masters` object with the specified attributes.
+     * @param name        The name of the masters student.
+     * @param address     The address of the  student.
+     * @param phoneNumber The phone number of the masters student.
+     * @param email       The email address of the masters student.
+     * @param idNumber    The ID number of the masters student.
+     * @param program     The program of the masters student.
+     */
 public class Masters extends Student {
     public Masters(String name, String address, String phoneNumber, String email, int idNumber, String program) {
         super(name, address, phoneNumber, email, idNumber, "Masters", program);
