46e48c718baf6efa22e6b6a74d23c6595c6d973f
Phoenixking900 <87982646+Phoenixking900@users.noreply.github.com>
2023-11-28 17:06:42 +0000

    Update Undergraduate.java

--- Undergraduate.java
+++ Undergraduate.java
@@ -1,3 +1,13 @@
+/**
+     * Constructs an `Undergraduate` object with the specified attributes.
+     *
+     * @param name        The name of the undergraduate student.
+     * @param address     The address of the undergraduate student.
+     * @param phoneNumber The phone number of the undergraduate student.
+     * @param email       The email address of the undergraduate student.
+     * @param idNumber    The ID number of the undergraduate student.
+     * @param program     The program of the undergraduate student.
+     */
 public class Undergraduate extends Student {
     public Undergraduate(String name, String address, String phoneNumber, String email, int idNumber, String program) {
         super(name, address, phoneNumber, email, idNumber, "Undergraduate", program);
