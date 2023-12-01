b5e595d8ca150bb2db338f482da9afaa003b1840
DriscoTuna <77690843+DriscoTuna@users.noreply.github.com>
2023-12-01 13:33:36 +0000

    Updated files

--- Department.java
+++ Department.java
@@ -1,29 +1,14 @@
 import java.util.ArrayList;
 public class Department
 {
-    private static final String FILE = "department.csv";
-
     String name;
     int deptID;
     ExaminationBoard examBoard; // for each dpartment
     ArrayList<Programme> programmes;
-    ArrayList<Modules> modules;
-
-    public Department(int deptID) {
-        this.deptID = deptID;
-        this.programmes = new ArrayList<Programme>();
-        this.modules = new ArrayList<Modules>();
+    public Department(String n, int ID) {
+        setName(n);
+        setID(ID);
     }
-
-    public Department(String name, int ID) {
-        this.name = name;
-        this.deptID = ID;
-        this.programmes = new ArrayList<Programme>();
-        this.modules = new ArrayList<Modules>();
-    }
-
-    public String getName() {return this.name;}
-    public int getID() {return this.deptID;}
     public void setName(String n) {this.name = n;}
     public void setID(int id) {this.deptID = id;}
     public void addProgramme(Programme prog) {
@@ -31,99 +16,16 @@ public class Department
             this.programmes.add(prog);
         }
     }
-    public void addModule(Modules module) {
-        if (!this.modules.contains(module)) {
-            this.modules.add(module);
+    public void examTime() {
+        for (Programme p : programmes) {
+            ExaminationBoard examBoard = new ExaminationBoard(p, p.passingQca); 
+            
         }
     }
-    // create exam board for all programmes
-
-    /**
-     * add the Department to the csv file
-     */
-    public void addToCsvFile() {
-        if ( !CSVFileManager.checkIfLineExistsInCSVFile(FILE, this.getID()) ) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(this.getID()).append(",").append(this.getName()).append(",");
-            sb.append(programmes.size()).append(",").append(modules.size());
-            for (Programme prog : this.programmes) {
-                sb.append(",").append(prog.getName());
-            }
-            for (Modules module : this.modules) {
-                sb.append(",").append(module.getCode());
-            }
-            int result = CSVFileManager.addLineToCSVFile(FILE, sb.toString());
-            if (result == 0) {
-                System.out.println("Department with name" + this.getID()+ " added successfully.");
-            } else {
-                System.out.println("Department with name" + this.getID()+ " could not be added.");
-            }
-        } else {
-            System.out.println("Department with name" + this.getID()+ " already exists.");
-        }
-    } 
-
-    public static boolean checkIfDepartmentExists(int id) {
-        return CSVFileManager.checkIfLineExistsInCSVFile(FILE, id);
-    }
-
-    /**
-     * remove the department from the csv file
-     * @param id the name of the department to be removed
-     */
-    public static int removeDepartmentFromCsv(int id){
-        if (checkIfDepartmentExists(id)){
-            return CSVFileManager.removeLineFromCSVFile(FILE, id);
-        }
-        return -1;
-    }
-
-    /**
-     * get the department from the csv file
-     * @param id the name of the department to be retrieved
-     */
-    public void getFromCsvFile(int id) {
-        if ( CSVFileManager.checkIfLineExistsInCSVFile(FILE, id) ) {
-            String line = CSVFileManager.getLineFromCSVFile(FILE, id);
-            if (line == null) {
-                System.out.println("Department with Name" + id + " does not exist.");
-            } else {
-                String[] parts = line.split(",");
-                this.setID(Integer.parseInt(parts[0]));
-                this.setName(parts[1]);
-                int numProgrammes = Integer.parseInt(parts[2]);
-                int numModules = Integer.parseInt(parts[3]);
-                for (int i = 0; i < numProgrammes; i++) {
-                    Programme prog = new Programme(parts[4 + i]);
-                    this.addProgramme(prog);
-                }
-                for (int i = 0; i < numModules; i++) {
-                    Modules module = new Modules(parts[4 + numProgrammes + i]);
-                    this.addModule(module);
-                }
-            }
-        } else {
-            System.out.println("Default with Name" + id + " does not exist.");
-        }
-    }
-
+    // What should this return?
     @Override
     public String toString() {
-        if (this.getName() == null) {
-            getFromCsvFile(this.getID());
-        }
-        StringBuilder sb = new StringBuilder();
-        sb.append("Department Name: ").append(this.getName()).append("\n");
-        sb.append("Department ID: ").append(this.getID()).append("\n");
-        sb.append("Programmes: ").append(this.programmes.size()).append("\n");
-        for (Programme prog : this.programmes) {
-            sb.append(prog.getName()).append("\n");
-        }
-        sb.append("Modules: ").append(this.modules.size()).append("\n");
-        for (Modules module : this.modules) {
-            sb.append(module.getCode()).append("\n");
-        }
-        return sb.toString();
+        String output = "";
+        return output;
     }
-    
 }
--- ExaminationBoard.java
+++ ExaminationBoard.java
@@ -9,13 +9,62 @@ import java.util.ArrayList;
  */
 public class ExaminationBoard
 {
-    public ExaminationBoard() {}
-    // check with Assessment class to get grade schema
-    // check each student QCA against schema for that module
-    // return pass or fail
+    // what data types do i need?
+    Programme programme;
+    double passingQca = 1.2; // stand in variable
+    ArrayList<Student> students;
+    public ExaminationBoard(Programme programme, double passingQca) {
+        this.programme = programme;
+        this.passingQca = passingQca;
+        this.students = getStudents();
+    }
+    public ArrayList<Student> getStudents() {return programme.getStudentsEnrolled();}
+    public double getQcaSem1(Student student) {return student.getQcaSem1();}
+    public double getQcaSem2(Student student) {return student.getQcaSem2();}
     public double getQca(Student student) {return student.getQcaYear();}
-    public boolean passFail() {
+    public boolean passFail(double qcaToCheck) {
         boolean pass = false;
+        if (qcaToCheck >= passingQca) {
+            pass = true;
+        }
         return pass;
+    }
+    // // 1 for sem1, 2 for sem2, 3 for year
+    public String checkQca(int assessmentPeriod, Student s){
+        String output = "";
+        if (assessmentPeriod < 0) {return "Please select Assessment Period";}
+        else if (assessmentPeriod == 0) { // Only results, no formatting (for CSVs)
+            output += String.format("%s,%s,%s", getQcaSem1(s), getQcaSem2(s), getQca(s));
+        } else if (assessmentPeriod == 1) { // Semester 1
+            if (passFail(getQcaSem1(s))) {
+                output += String.format("%f - PASS", getQcaSem1(s));
+            } else {
+                output += String.format("%f - FAIL", getQcaSem1(s));
+            }
+        } else if (assessmentPeriod == 2) { // Semester 2
+            if (passFail(getQcaSem2(s))) {
+                output += String.format("%f - PASS", getQcaSem2(s));
+            } else {
+                output += String.format("%f - FAIL", getQcaSem2(s));
+            }
+        } else if (assessmentPeriod == 3) { // Year
+            if (passFail(getQca(s))) {
+                output += String.format("%f - PASS", getQca(s));
+            }else {
+                output += String.format("%f - FAIL", getQca(s));
+            }
+        }
+        return output;
+    }
+    
+    @Override
+    public String toString(){ // display results for entire programme
+        String output = String.format("%s", programme.toString());
+        for (Student s : students) {
+            output += String.format("%d(%s)\n%s", s.getIdNumber(), s.getName());
+            output += String.format("Sem 1: %s\nSem2: %s\nYear : %s\n",
+                checkQca(1,s), checkQca(2,s), checkQca(3,s));
+        }
+        return output;
     }
 }
